6e85cfec653102352d59dbd6139ff551e4646ed6
James Collier <jamesc@cakesolutions.net>
2018-01-20 15:39:54 +0000

    implement level 2

--- core/src/test/scala/uritemplate/UriTemplateTests.scala
+++ core/src/test/scala/uritemplate/UriTemplateTests.scala
@@ -4,6 +4,17 @@ import utest._
 
 object UriTemplateTests extends TestSuite {
 
+  private def test(
+                  vars: Map[String, String])(
+                  templatetWithExp: (String, String)*
+                  ): Unit = {
+    val varList = vars.toList
+    templatetWithExp.foreach { case (template, exp) =>
+      val actual = UriTemplate(template).expand(varList: _*)
+      assert(exp == actual)
+    }
+  }
+
   val tests = Tests {
     /**
       * .-----------------------------------------------------------------.
@@ -21,10 +32,57 @@ object UriTemplateTests extends TestSuite {
       * |     |    {hello}               Hello%20World%21                 |
       * '-----------------------------------------------------------------'
       */
-    "level 1" - {
-      val vars = Map("var" -> "value", "hello" -> "Hello World!").toList
-      assert(UriTemplate("{var}").expand(vars: _*) == "value")
-      assert(UriTemplate("{hello}").expand(vars: _*) == "Hello%20World%21")
+    'level1 - {
+      val vars = Map(
+        "var" -> "value",
+        "hello" -> "Hello World!"
+      )
+      "Simple string expansion" - test(vars)(
+        "{var}" -> "value",
+        "{hello}" -> "Hello%20World%21"
+      )
+    }
+
+    /**
+      * .-----------------------------------------------------------------.
+      * | Level 2 examples, with variables having values of               |
+      * |                                                                 |
+      * |             var   := "value"                                    |
+      * |             hello := "Hello World!"                             |
+      * |             path  := "/foo/bar"                                 |
+      * |                                                                 |
+      * |-----------------------------------------------------------------|
+      * | Op       Expression            Expansion                        |
+      * |-----------------------------------------------------------------|
+      * |  +  | Reserved string expansion                     (Sec 3.2.3) |
+      * |     |                                                           |
+      * |     |    {+var}                value                            |
+      * |     |    {+hello}              Hello%20World!                   |
+      * |     |    {+path}/here          /foo/bar/here                    |
+      * |     |    here?ref={+path}      here?ref=/foo/bar                |
+      * |-----+-----------------------------------------------------------|
+      * |  #  | Fragment expansion, crosshatch-prefixed       (Sec 3.2.4) |
+      * |     |                                                           |
+      * |     |    X{#var}               X#value                          |
+      * |     |    X{#hello}             X#Hello%20World!                 |
+      * '-----------------------------------------------------------------'
+      */
+    'level2 - {
+      val vars = Map(
+        "var" -> "value",
+        "hello" -> "Hello World!",
+        "path" -> "/foo/bar"
+      )
+      "Reserved string expansion" - test(vars)(
+        "{+var}" -> "value",
+        "{+hello}" -> "Hello%20World!",
+        "{+path}/here" ->"/foo/bar/here",
+        "here?ref={+path}" -> "here?ref=/foo/bar"
+      )
+      "Fragment expansion, crosshatch-prefixed" - test(vars)(
+        "X{#var}" -> "X#value",
+        "X{#hello}" -> "X#Hello%20World!"
+      )
     }
   }
 }
