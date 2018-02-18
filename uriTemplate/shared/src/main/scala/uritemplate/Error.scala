90a5102c145a144643672fa3747b9c41f5e84114
James Collier <James.Collier@bamtechmedia.com>
2018-02-18 22:39:21 +0000

    spelling

--- uriTemplate/shared/src/main/scala/uritemplate/Error.scala
+++ uriTemplate/shared/src/main/scala/uritemplate/Error.scala
@@ -6,7 +6,7 @@ sealed trait Error {
 object Error {
   /** The supplied template could not be parsed correctly. **/
   final case class MalformedUriTemplate(override val message: String) extends Error
-  /** Prefix modifer used with a non string value. **/
+  /** Prefix modifier used with a non string value. **/
   final case class InvalidCombination(override val message: String) extends Error
 }
 
