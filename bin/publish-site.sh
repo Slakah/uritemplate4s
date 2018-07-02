f11e0f2f75695d38e3732a71a894e20d35a46362
JCollier <Slakah@users.noreply.github.com>
2018-07-02 12:27:43 +0100

    Revert "skip ci on site publish (#9)" (#10)
    This reverts commit 5b9c21a1ffc9d9dc333fa6139860bf51de20aa73.

--- bin/publish-site.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -ue
-export SBT_GHPAGES_COMMIT_MESSAGE="updated site [ci skip]"
-
-exec ";set docs/git.remoteRepo := \"https://$GITHUB_TOKEN@github.com/Slakah/uritemplate4s.git\";publishMicrosite"
