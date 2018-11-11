aacc89a195540d640f34059189088ad4f56a6665
JCollier <Slakah@users.noreply.github.com>
2018-11-11 12:32:58 +0000

    Update to scalafix 0.9.0 (#37)

--- core/shared/src/main/scala/uritemplate4s/syntax/package.scala
+++ core/shared/src/main/scala/uritemplate4s/syntax/package.scala
@@ -2,7 +2,7 @@ package uritemplate4s
 
 package object syntax {
 
-  implicit final class ToValueOps[A](val a: A) extends AnyVal {
+  implicit final class ToValueOps[A](private val a: A) extends AnyVal {
     def toValue(implicit toValue: ToValue[A]): Value = toValue(a)
   }
 }
