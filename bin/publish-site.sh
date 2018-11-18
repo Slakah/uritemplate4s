f679c2202f808311c88353ed338eeba9e0b451c6
JCollier <Slakah@users.noreply.github.com>
2018-11-18 23:34:06 +0000

    Publish tags (#45)

--- bin/publish-site.sh
+++ bin/publish-site.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -ue
+set -ue -o pipefail
 export SBT_GHPAGES_COMMIT_MESSAGE="updated site [ci skip]"
 
 exec sbt ";set docs/git.remoteRepo := \"https://$GITHUB_TOKEN@github.com/Slakah/uritemplate4s.git\";publishMicrosite"
