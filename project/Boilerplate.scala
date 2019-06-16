41e1a7fba0918ab6579190ab4432d5bfdf5e3a21
JCollier <Slakah@users.noreply.github.com>
2019-06-16 22:09:40 +0100

    0-arity expand (#77)
    This is for cases where no arguments are needed

--- project/Boilerplate.scala
+++ project/Boilerplate.scala
@@ -15,13 +15,13 @@ object Boilerplate {
     """.stripMargin
 
   private val content = template(
-    (1 to 22).map { numArities =>
+    ("def expand(): ExpandResult = expandVars()" :: (1 to 22).toList.map { numArities =>
       val range = 1 to numArities
       val genericTypes = range.map(i => s"A$i : ToValue").mkString(", ")
       val args = range.map(i => s"a$i: (String, A$i)").mkString(", ")
       val params = range.map(i => s"a$i._1 -> a$i._2.toValue").mkString(", ")
       s"def expand[$genericTypes]($args): ExpandResult = expandVars($params)"
-    }.mkString("\n")
+    }).mkString("\n")
   )
 
   def gen(dir: File): List[File] = {
