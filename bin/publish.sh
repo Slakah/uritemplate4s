b0b5aa6efe3a14a19120f1b8e4e71cfbadaee106
James Collier <jrc.collier@gmail.com>
2019-09-08 23:18:49 +0100

    fix publish scripts sbt output

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-set -ue -o pipefail
+set -euo pipefail
 
 readonly iv='46E8A1968B174645178FA13AA8F7CFE9'
 
 openssl aes-256-cbc -K "$ENCRYPTED_KEY" -iv $iv -in secring.asc.enc -out secring.asc -d
 
-exec sbt "; clean; +releaseEarly"
+sbt "; clean; +releaseEarly"
