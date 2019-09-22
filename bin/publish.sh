fad1c9f6589db02bb20373cf0ed9c2fe5b1a0e07
James Collier <Slakah@users.noreply.github.com>
2019-09-22 21:20:52 +0100

    Replace sbt-release-early with sbt-sontatype + sbt-pgp (#114)
    * sbt-release-early replaced
    
    * master only publish

--- bin/publish.sh
+++ bin/publish.sh
@@ -5,4 +5,6 @@ readonly iv='46E8A1968B174645178FA13AA8F7CFE9'
 
 openssl aes-256-cbc -K "$ENCRYPTED_KEY" -iv $iv -in secring.asc.enc -out secring.asc -d
 
-sbt "; clean; +releaseEarly"
+gpg --batch --passphrase $PGP_PASSPHRASE --allow-secret-key-import --import secring.asc
+
+sbt "; clean; sonatypeDropAll; +coreJVM/publishSigned; sonatypeBundleRelease"
